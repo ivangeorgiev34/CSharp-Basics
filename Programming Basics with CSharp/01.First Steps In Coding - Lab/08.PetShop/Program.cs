9a24377e022475a4b8d982b8cb60848a994bbeff
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2022-12-17 15:23:43 +0200

    Added general file and homework to it

---
+++ Programming Basics with CSharp/01.First Steps In Coding - Lab/08.PetShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08.PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPacketsOfDogs = int.Parse(Console.ReadLine());
+            int numberOfPacketsOfCats = int.Parse(Console.ReadLine());
+            double finalPriceDogs = numberOfPacketsOfDogs * 2.50;
+            int finalPriceCats = numberOfPacketsOfCats * 4;
+            double totalPrice = finalPriceDogs + finalPriceCats;
+
+            Console.WriteLine($"{totalPrice} lv.");
+        }
+    }
+}
