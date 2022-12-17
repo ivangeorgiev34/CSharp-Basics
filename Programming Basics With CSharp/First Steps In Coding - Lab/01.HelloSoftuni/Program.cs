fe1d12286a2544671376658d678431ce4f8d4f2f
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2022-12-17 15:33:42 +0200

    Added First Steps In Coding Homerwork

---
+++ Programming Basics With CSharp/First Steps In Coding - Lab/01.HelloSoftuni/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.HelloSoftUni
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Programming Basics With CSharp/First Steps In Coding - Lab/09.YardGreening/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09.YardGreening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double pricePerMeter = meters * 7.61;
+            double discount = pricePerMeter * 0.18;
+            double total = pricePerMeter - discount;
+            Console.WriteLine($"The final price is: {total} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
