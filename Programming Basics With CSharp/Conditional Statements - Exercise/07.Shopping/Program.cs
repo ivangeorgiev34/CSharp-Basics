c1519ec1267771ddebb4c7a4c3edded0d45d004f
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-01 19:25:08 +0200

    Added exercise 7

---
+++ Programming Basics With CSharp/Conditional Statements - Exercise/07.Shopping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07.Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int videocards = int.Parse(Console.ReadLine());
+            int processors = int.Parse(Console.ReadLine());
+            int rams = int.Parse(Console.ReadLine());
+
+            double videocardSum = 250 * videocards;
+            double processorSum = (videocardSum * 0.35) * processors;
+            double ramSum = (videocardSum * 0.10) * rams;
+            double productsSum = videocardSum + processorSum + ramSum;
+            if (videocards > processors)
+            {
+                productsSum = productsSum - (productsSum * 0.15);
+            }
+
+            if (budget >= productsSum)
+            {
+                Console.WriteLine($"You have {(budget - productsSum):F2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {(productsSum - budget):F2} leva more!");
+            }
+        }
+    }
+}
