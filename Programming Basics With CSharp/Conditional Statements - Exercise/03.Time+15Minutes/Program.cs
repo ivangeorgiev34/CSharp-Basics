898549c03c8209da7d030806637fe30d7d0aaed8
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-01 19:23:38 +0200

    Added exercise 3

---
+++ Programming Basics With CSharp/Conditional Statements - Exercise/03.Time+15Minutes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _03.Time_15Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int minutesSum = minutes + 15;
+
+
+
+            if (hour != 23 && minutesSum >= 60 && (minutesSum % 60) < 10)
+            {
+
+                Console.WriteLine($"{hour + 1}:0{minutesSum % 60}");
+
+            }
+            else if (hour != 23 && minutesSum >= 60)
+            {
+                Console.WriteLine($"{hour + 1}:{minutesSum % 60}");
+            }
+            else if (hour != 23 && minutesSum < 60)
+            {
+                Console.WriteLine($"{hour}:{minutesSum}");
+
+            }
+            else if (minutesSum < 60)
+            {
+                Console.WriteLine($"{hour}:{minutesSum}");
+            }
+            else if (minutesSum >= 60 && (minutesSum % 60) < 10)
+            {
+                Console.WriteLine($"0:0{minutesSum % 60}");
+            }
+            else
+            {
+                Console.WriteLine($"0:{minutesSum % 60}");
+            }
+        }
+    }
+}
