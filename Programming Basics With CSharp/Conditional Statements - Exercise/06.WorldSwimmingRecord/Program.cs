702db3026e059d4e1f87f9d7de4b69adb3e9e380
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-01 19:24:45 +0200

    Added exercise 6

---
+++ Programming Basics With CSharp/Conditional Statements - Exercise/06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06.WorldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double lenght = double.Parse(Console.ReadLine());
+            double meterInTime = double.Parse(Console.ReadLine());
+            double timeIvan = lenght * meterInTime;
+            double resistence = Math.Floor(lenght / 15) * 12.5;
+            double totalTimeIvan = timeIvan + resistence;
+            if (totalTimeIvan < record)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTimeIvan:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalTimeIvan - record:F2} seconds slower.");
+            }
+        }
+    }
+}
