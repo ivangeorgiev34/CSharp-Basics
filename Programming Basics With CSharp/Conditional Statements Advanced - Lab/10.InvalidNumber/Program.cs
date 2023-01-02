3761476f791571c65ac87bea135d3f0930699e69
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-02 22:23:54 +0200

    Added exercise 10

---
+++ Programming Basics With CSharp/Conditional Statements Advanced - Lab/10.InvalidNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10.InvalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            bool check = (num >= 100 && num <= 200) || num == 0;
+            if (!check)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
