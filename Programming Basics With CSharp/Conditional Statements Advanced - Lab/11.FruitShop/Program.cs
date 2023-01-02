9daa4aebf88399af3b76bd179dced35ccb019dd5
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-02 22:24:50 +0200

    Added exercise 11

---
+++ Programming Basics With CSharp/Conditional Statements Advanced - Lab/11.FruitShop/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _11.FruitShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0.0;
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
+            {
+                switch (fruit)
+                {
+                    case "banana":
+                        price = 2.5;
+                        break;
+                    case "apple":
+                        price = 1.2;
+                        break;
+                    case "orange":
+                        price = 0.85;
+                        break;
+                    case "grapefruit":
+                        price = 1.45;
+                        break;
+                    case "kiwi":
+                        price = 2.7;
+                        break;
+                    case "pineapple":
+                        price = 5.5;
+                        break;
+                    case "grapes":
+                        price = 3.85;
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                    
+                }
+                price = price * quantity;
+                Console.WriteLine($"{price:F2}");
+            }
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                switch (fruit)
+                {
+                    case "banana":
+                        price = 2.7;
+                        break;
+                    case "apple":
+                        price = 1.25;
+                        break;
+                    case "orange":
+                        price = 0.9;
+                        break;
+                    case "grapefruit":
+                        price = 1.6;
+                        break;
+                    case "kiwi":
+                        price = 3;
+                        break;
+                    case "pineapple":
+                        price = 5.6;
+                        break;
+                    case "grapes":
+                        price = 4.2;
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;  
+                }
+                price = price * quantity;
+                Console.WriteLine($"{price:F2}");
+
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
