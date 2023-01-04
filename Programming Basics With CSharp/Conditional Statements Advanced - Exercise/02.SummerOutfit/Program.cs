d1605ad6e3ae01d1c30d3567e0a26a0be8774cd8
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-04 21:55:30 +0200

    Added exercise 2

---
+++ Programming Basics With CSharp/Conditional Statements Advanced - Exercise/02.SummerOutfit/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _02.SummerOutfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+
+            string outfit = "";
+            string shoes = "";
+
+            switch (time)
+            {
+                case "Morning":
+                    if (10 <= degrees && degrees <= 18)
+                    {
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                    }
+                    else if (18 < degrees && degrees <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    break;
+                case "Afternoon":
+                    if (10 <= degrees && degrees <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (18 < degrees && degrees <= 24)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    else
+                    {
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                    }
+                    break;
+                case "Evening":
+                    if (10 <= degrees && degrees <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (18 < degrees && degrees <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    break;
+            }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
