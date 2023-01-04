38dbfeb041e4bb3075dda1e8b902fc5a43e70574
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-04 21:54:50 +0200

    Added exercise 1

---
+++ Programming Basics With CSharp/Conditional Statements Advanced - Exercise/01.Cinema/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectionType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            double total = 0.0;
+
+            switch (projectionType)
+            {
+                case "Premiere":
+                    total = 12 * rows * columns;
+                    break;
+                case "Normal":
+                    total = 7.5 * rows * columns;
+                    break;
+                case "Discount":
+                    total = 5 * rows * columns;
+                    break;
+
+            }
+            Console.WriteLine($"{total:F2} leva");
+        }
+    }
+}
