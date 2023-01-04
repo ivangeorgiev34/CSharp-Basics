da53a3fb522c5b4642134b5c78acac02f712820c
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-04 21:58:21 +0200

    Added exercise 08.OnTimeForTheExam

---
+++ Programming Basics With CSharp/Conditional Statements Advanced - Exercise/08.OnTimeForTheExam/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _08.OnTimeForTheExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHour = int.Parse(Console.ReadLine());
+            int examMinutes = int.Parse(Console.ReadLine());
+            int arrivalHour = int.Parse(Console.ReadLine());
+            int arrivalMinutes = int.Parse(Console.ReadLine());
+
+            int examHourIntoMinutes = 60 * examHour;
+            int arrivalHourIntoMinutes = 60 * arrivalHour;
+            string state = "";
+            int minutes = 0;
+            int hours = 0;
+
+            if ((examHourIntoMinutes + examMinutes) >= (arrivalHourIntoMinutes + arrivalMinutes))
+            {
+                if (((examHourIntoMinutes + examMinutes) - (arrivalHourIntoMinutes + arrivalMinutes)) >= 60)
+                {
+                    state = "Early";
+                    hours = ((examHourIntoMinutes + examMinutes) - (arrivalHourIntoMinutes + arrivalMinutes)) / 60;
+                    minutes = ((examHourIntoMinutes + examMinutes) - (arrivalHourIntoMinutes + arrivalMinutes)) % 60;
+                    Console.WriteLine(state);
+                    if (minutes == 0)
+                    {
+                        Console.WriteLine($"{hours}:00 hours before the start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hours}:{minutes} hours before the start");
+                    }
+                }
+                else if ((examHourIntoMinutes + examMinutes) - (arrivalHourIntoMinutes + arrivalMinutes) > 30)
+                {
+                    state = "Early";
+                    minutes = (examHourIntoMinutes + examMinutes) - (arrivalHourIntoMinutes + arrivalMinutes);
+                    Console.WriteLine(state);
+                    Console.WriteLine($"{minutes} minutes before the start");
+                }
+                else
+                {
+                    if (((examHourIntoMinutes + examMinutes) - (arrivalHourIntoMinutes + arrivalMinutes)) == 0)
+                    {
+                        state = "On time";
+                        Console.WriteLine(state);
+                    }
+                    else
+                    {
+                        state = "On time";
+                        minutes = (examHourIntoMinutes + examMinutes) - (arrivalHourIntoMinutes + arrivalMinutes);
+                        Console.WriteLine(state);
+                        Console.WriteLine($"{minutes} minutes before the start");
+                    }
+                }
+            }
+            else if ((examHourIntoMinutes + examMinutes) < (arrivalHourIntoMinutes + arrivalMinutes))
+            {
+                if ((arrivalHourIntoMinutes + arrivalMinutes) - (examHourIntoMinutes + examMinutes) >= 60)
+                {
+                    state = "Late";
+                    hours = ((arrivalHourIntoMinutes + arrivalMinutes) - (examHourIntoMinutes + examMinutes)) / 60;
+                    minutes = ((arrivalHourIntoMinutes + arrivalMinutes) - (examHourIntoMinutes + examMinutes)) % 60;
+                    Console.WriteLine(state);
+                    if (minutes == 0)
+                    {
+                        Console.WriteLine($"{hours}:00 hours after the start");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hours}:{minutes} hours after the start");
+                    }
+                }
+                else
+                {
+                    state = "Late";
+                    minutes = (arrivalHourIntoMinutes + arrivalMinutes) - (examHourIntoMinutes + examMinutes);
+                    Console.WriteLine(state);
+                    Console.WriteLine($"{minutes} minutes after the start");
+                }
+            }
+        }
+    }
+}
