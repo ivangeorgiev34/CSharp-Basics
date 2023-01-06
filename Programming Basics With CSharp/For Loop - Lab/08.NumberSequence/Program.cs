ef64f12e7f9e6bddd571612fed752a504c9e0d0b
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-06 14:53:23 +0200

    Added exercise 08.NumberSequence

---
+++ Programming Basics With CSharp/For Loop - Lab/08.NumberSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int MaxNumber = int.MinValue;
+            int MinNumber = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > MaxNumber)
+                {
+                    MaxNumber = number;
+                }
+                if (number < MinNumber)
+                {
+                    MinNumber = number;
+                }
+            }
+            Console.WriteLine($"Max number: {MaxNumber}");
+            Console.WriteLine($"Min number: {MinNumber}");
+        }
+    }
+}
