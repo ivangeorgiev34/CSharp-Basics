dc2f4cae64a763dce211f7ba40d91a3458d1f1cf
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-08 23:03:29 +0200

    Added exercise 08.TennisRanklist

---
+++ Programming Basics With CSharp/For Loop - Exercise/08.TennisRanklist/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _08.TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournamentsPlayed = int.Parse(Console.ReadLine());
+            int pointFromBeggining = int.Parse(Console.ReadLine());
+
+            double points = 0;
+            double wonTournaments = 0;
+
+            for (int i = 1; i <= tournamentsPlayed; i++)
+            {
+                string stage = Console.ReadLine();
+                if (stage == "W")
+                {
+                    wonTournaments++;
+                    pointFromBeggining += 2000;
+                    points += 2000;
+                }
+                else if (stage == "F")
+                {
+                    pointFromBeggining += 1200;
+                    points += 1200;
+                }
+                else if (stage == "SF")
+                {
+                    pointFromBeggining += 720;
+                    points += 720;
+                }
+
+            }
+
+            Console.WriteLine($"Final points: {pointFromBeggining}");
+            Console.WriteLine($"Average points: {Math.Floor(points / tournamentsPlayed)}");
+            Console.WriteLine($"{(wonTournaments / tournamentsPlayed) * 100:f2}%");
+        }
+    }
+}
