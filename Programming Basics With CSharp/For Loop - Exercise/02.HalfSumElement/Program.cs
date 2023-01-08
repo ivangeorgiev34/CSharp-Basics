86b15cd4972a4cd7b26e364dadf52b8dbf8bc09c
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-08 23:01:06 +0200

    Added exercise 02.HalfSumElement

---
+++ Programming Basics With CSharp/For Loop - Exercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNumber = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+
+
+
+            }
+            int sumWithoutMaxNumber = sum - maxNumber;
+            int difference = Math.Abs(maxNumber - sumWithoutMaxNumber);
+            if (sumWithoutMaxNumber == maxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumWithoutMaxNumber}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {difference}");
+            }
+        }
+    }
+}
