0fb809d6d0a4285ddd805c6e6da12543baa777f1
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-08 23:02:23 +0200

    Added exercise 05.Salary

---
+++ Programming Basics With CSharp/For Loop - Exercise/05.Salary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05.Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfTabs; i++)
+            {
+                string tabName = Console.ReadLine();
+                if (tabName == "Facebook")
+                {
+                    salary = salary - 150;
+                }
+                else if (tabName == "Instagram")
+                {
+                    salary = salary - 100;
+                }
+                else if (tabName == "Reddit")
+                {
+                    salary = salary - 50;
+                }
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine($"You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
