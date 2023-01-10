a55b00b75c74d6c3a001003434ee198cf81372de
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-10 15:31:11 +0200

    Added exercise 03.SumNumbers

---
+++ Programming Basics With CSharp/While Loop - Lab/03.SumNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (sum <= number)
+            {
+                sum += int.Parse(Console.ReadLine());
+                if (sum >= number)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
