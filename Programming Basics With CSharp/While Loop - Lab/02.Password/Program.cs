b350f7e503df8c274919ae0abf57aa10dbb54ee4
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-10 15:30:46 +0200

    Added exercise 02.Password

---
+++ Programming Basics With CSharp/While Loop - Lab/02.Password/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            string passwordAttempt = Console.ReadLine();
+
+            while (passwordAttempt != password)
+            {
+                passwordAttempt = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
