3d9fbc950e122b8cd278a18333bc037524bf141b
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-10 15:32:24 +0200

    Added exercise 06.MaxNumber

---
+++ Programming Basics With CSharp/While Loop - Lab/06.MaxNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int maxNumber = int.MinValue;
+            while (input != "Stop")
+            {
+
+                int inputInInt = int.Parse(input);
+                if (inputInInt > maxNumber)
+                {
+                    maxNumber = inputInInt;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
