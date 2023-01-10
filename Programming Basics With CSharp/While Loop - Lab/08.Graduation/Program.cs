a452a13041b020b8b96f06bbd02eed88b88a4ae0
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-10 15:33:37 +0200

    Added exercise 08.Graduation

---
+++ Programming Basics With CSharp/While Loop - Lab/08.Graduation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08.Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameStudent = Console.ReadLine();
+            int counter = 0;
+            int expeledCounter = 0;
+            double gradeSum = 0;
+
+            while (counter < 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade < 4)
+                {
+                    expeledCounter++;
+                    if (expeledCounter == 2)
+                    {
+                        Console.WriteLine($"{nameStudent} has been excluded at {counter} grade");
+                        return;
+                    }
+                }
+
+                counter++;
+                gradeSum += grade;
+
+                if (counter == 12)
+                {
+                    Console.WriteLine($"{nameStudent} graduated. Average grade: {gradeSum / 12:f2}");
+
+                }
+            }
+        }
+    }
+}
