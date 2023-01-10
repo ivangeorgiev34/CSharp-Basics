5bfd62faab76546cea98b0b4e4096cac4839adab
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-10 15:33:17 +0200

    Added exercise 07.MinNumber

---
+++ Programming Basics With CSharp/While Loop - Lab/07.MinNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int minNumber = int.MaxValue;
+            while (command != "Stop")
+            {
+                int commandInt = int.Parse(command);
+                if (commandInt < minNumber)
+                {
+                    minNumber = commandInt;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
