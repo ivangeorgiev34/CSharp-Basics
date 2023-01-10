f3dd7a36679a33c81040a4c2b57e165ea4587fd2
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-10 15:31:34 +0200

    Added exercise 04.Sequence2k+1

---
+++ Programming Basics With CSharp/While Loop - Lab/04.Sequence2k+1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04.Sequence2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 1;
+            while (a <= n)
+            {
+                Console.WriteLine(a);
+                a = a * 2 + 1;
+            }
+        }
+    }
+}
