8556be5b9887c5dfca9dc2932dc6e0055000d5bf
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-10 15:32:01 +0200

    Added exercise 05.AccountBalance

---
+++ Programming Basics With CSharp/While Loop - Lab/05.AccountBalance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fee = Console.ReadLine();
+            double total = 0;
+
+            while (fee != "NoMoreMoney")
+            {
+                double ammount = double.Parse(fee);
+
+                if (ammount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                total += ammount;
+                Console.WriteLine($"Increase: {ammount:f2}");
+                fee = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {total:f2}");
+        }
+    }
+}
