ba04dc09f253fc5538905dc2b30a5f4d22607bce
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-12 17:07:42 +0200

    Added exercise 01.OldBooks

---
+++ Programming Basics With CSharp/While Loop - Exercise/01.OldBooks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedBook = Console.ReadLine();
+            int counter = 0;
+            string book = Console.ReadLine(); 
+
+            while (book != "No More Books")
+            {
+
+                if (searchedBook == book)
+                {
+                    Console.WriteLine($"You checked {counter} books and found it.");
+                    break;
+                }
+                counter++;
+                book = Console.ReadLine();
+                if (book == "No More Books")
+                {
+                    Console.WriteLine($"The book you search is not here!");
+                    Console.WriteLine($"You checked {counter} books.");
+                    break;
+                }
+            }
+        }
+    }
+}
