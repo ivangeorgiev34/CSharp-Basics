fdf69e409dbe2d82ca33365cd22f023d569df9c5
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-12 17:09:35 +0200

    Added exercise 05.Coins

---
+++ Programming Basics With CSharp/While Loop - Exercise/05.Coins/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _05.Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double change = 100 * double.Parse(Console.ReadLine());
+            int counter = 0;
+
+            // 2 , 1 , 0.50 , 0.20 , 0.10 , 0.05 , 0.02, 0.01
+
+            while (change > 0)
+            {
+                if (change >= 200)
+                {
+                    change = change - 200;
+                    counter++;
+                }
+                else if (change >= 100)
+                {
+                    change = change - 100;
+                    counter++;
+                }
+                else if (change >= 50)
+                {
+                    change = change - 50;
+                    counter++;
+                }
+                else if (change >= 20)
+                {
+                    change = change - 20;
+                    counter++;
+                }
+                else if (change >= 10)
+                {
+                    change = change - 10;
+                    counter++;
+                }
+                else if (change >= 5)
+                {
+                    change = change - 5;
+                    counter++;
+                }
+                else if (change >= 2)
+                {
+                    change = change - 2;
+                    counter++;
+                }
+                else if (change >= 1)
+                {
+                    change = change - 1;
+                    counter++;
+                }
+                else
+                {
+                    change = 0;
+
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
