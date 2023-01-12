03f0f12d31b35677adb0f385934e769ee5230c15
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-12 17:09:59 +0200

    Added exercise 06.Cake

---
+++ Programming Basics With CSharp/While Loop - Exercise/06.Cake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+
+            int numPieces = width * lenght;
+            string command = string.Empty;
+            int pieces = 0;
+
+            while (command != "STOP")
+            {
+                command = Console.ReadLine();
+
+
+                if (command == "STOP")
+                {
+                    Console.WriteLine($"{numPieces} pieces are left.");
+                    break;
+                }
+                numPieces = numPieces - int.Parse(command);
+                pieces = pieces + int.Parse(command);
+                if (numPieces < 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {pieces - (width * lenght)} pieces more.");
+                    break;
+                }
+            }
+        }
+    }
+}
