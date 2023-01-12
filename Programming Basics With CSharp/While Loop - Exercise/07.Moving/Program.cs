425f37d5d46bcf06eadf41e40290fb9390547c7c
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-12 17:10:20 +0200

    Added exercise 07.Moving

---
+++ Programming Basics With CSharp/While Loop - Exercise/07.Moving/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07.Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            string command = null;
+            int sumCubicMeters = 0;
+
+            while (command != "Done")
+            {
+                command = Console.ReadLine();
+                if (command == "Done")
+                {
+                    Console.WriteLine($"{(width * lenght * height) - sumCubicMeters} Cubic meters left.");
+                    break;
+                }
+                sumCubicMeters = sumCubicMeters + int.Parse(command);
+                if (sumCubicMeters > (width * lenght * height))
+                {
+                    Console.WriteLine($"No more free space! You need {sumCubicMeters - (width * lenght * height)} Cubic meters more.");
+                    break;
+                }
+            }
+        }
+    }
+}
