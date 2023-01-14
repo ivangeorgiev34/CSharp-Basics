791602177e33aa678523dc32f94a9fee30695109
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-14 17:06:08 +0200

    Added exercise 04.SumOfTwoNumbers

---
+++ Programming Basics With CSharp/Nested Loops - Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int number = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    counter++;
+                    if (i + j == number)
+                    {
+
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {number})");
+                        return;
+                    }
+                }
+
+            }
+            Console.WriteLine($"{counter} combinations - neither equals {number}");
+        }
+    }
+}
