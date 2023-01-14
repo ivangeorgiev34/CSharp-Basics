19b985bf766f786b181e8d8efe5d6d1a2d72f441
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-14 17:04:12 +0200

    Added exercise 01.Clock

---
+++ Programming Basics With CSharp/Nested Loops - Lab/01.Clock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.Clock
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int p = 0; p <= 59; p++)
+                {
+                    Console.WriteLine($"{i}:{p}");
+                }
+            }
+        }
+    }
+}
