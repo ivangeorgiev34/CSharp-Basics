ec724baeba496744b22bab011772041a094d6e16
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-14 17:05:21 +0200

    Added exercise 02.MultiplicationTable

---
+++ Programming Basics With CSharp/Nested Loops - Lab/02.MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.MultiplicationTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int p = 1; p <= 10; p++)
+                {
+                    Console.WriteLine($"{i} * {p} = {i * p}");
+                }
+            }
+        }
+    }
+}
