b003193fe039b9eeb2d6d6c91f3fe1629df301e6
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-14 17:05:46 +0200

    Added exercise 03.Combinations

---
+++ Programming Basics With CSharp/Nested Loops - Lab/03.Combinations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.Combinations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counterValid = 0;
+            for (int x = 0; x <= n; x++)
+            {
+                for (int y = 0; y <= n; y++)
+                {
+                    for (int z = 0; z <= n; z++)
+                    {
+                        if (x + y + z == n)
+                        {
+                            counterValid++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counterValid);
+        }
+    }
+}
